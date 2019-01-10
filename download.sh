ef240795c5676789fc0ebc5f2cc10a946a862ff5
Yong Hao TENG <yonghao.teng@gmail.com>
2019-01-10 17:00:28 +0800

    Update travis config

--- download.sh
+++ download.sh
@@ -1,9 +1,4 @@
 curl -s https://api.github.com/repos/reposense/RepoSense/releases/latest \
 | grep "browser_download_url.*jar" \
 | cut -d \" -f 4 \
-| wget -qi -
-
-if [[ $? -ne 0 ]]; then
-    echo "Unable to download RepoSense."
-    exit 1; 
-fi
+| wget -i - -O RepoSense.jar
