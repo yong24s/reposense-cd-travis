d087230dde719facbe869df76ababf6a12c9bc50
Yong Hao TENG <yonghao.teng@gmail.com>
2019-01-10 23:47:01 +0800

    Update download script to fallback to use existing jar

--- download.sh
+++ download.sh
@@ -1,4 +1,12 @@
-curl -s https://api.github.com/repos/reposense/RepoSense/releases/latest \
+#!/bin/bash
+
+# Use github api to get download link of the latest release
+URL=$(curl -s https://api.github.com/repos/reposense/RepoSense/releases/latest \
 | grep "browser_download_url.*jar" \
-| cut -d \" -f 4 \
-| wget -i - -O RepoSense.jar
+| cut -d \" -f 4)
+
+# If URL does not contain the file, the api request may be rate controlled
+# So fallback and use existing jar file
+if [[ $URL == *"RepoSense.jar"* ]]; then
+  wget $URL
+fi
